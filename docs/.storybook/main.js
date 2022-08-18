8b054f66369be112fe7449683c9e708acbb8644e
gigibean <gigibean@naver.com>
2022-08-19 00:41:54 +0900

    [docs] - feat: create package && initial setting
    - storybook
    - firebase

---
+++ docs/.storybook/main.js
@@ -0,0 +1,15 @@
+module.exports = {
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  framework: '@storybook/react',
+  core: {
+    builder: 'webpack5',
+  },
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.(ts|tsx)$/,
+      loader: require.resolve('babel-loader'),
+    });
+    return config;
+  },
+};
