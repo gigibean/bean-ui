068a9f11a5c1ae65a94be5ac2ad7d735fea3bca5
gigibean <gigibean@naver.com>
2022-08-20 22:05:32 +0900

    [docs] - feat: edit button, loader stories & edit setting

--- docs/.storybook/preview.js
+++ docs/.storybook/preview.js
@@ -1,9 +1,13 @@
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  viewMode: 'docs',
+  previewTabs: {
+    canvas: { hidden: true },
+  },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
