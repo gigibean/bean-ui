b617254da430f0db93eb34af0a30cfb925bed2d1
gigibean <gigibean@naver.com>
2022-08-20 22:05:32 +0900

    [docs] - feat: edit button, loader stories & edit setting

--- docs/src/components/Button.stories.tsx
+++ docs/src/components/Button.stories.tsx
@@ -30,12 +30,19 @@ export default {
         'deepOrange',
         'brown',
         'gray',
+        '#FFB3B3',
+        'rgb(63, 167, 150)',
       ],
+    },
+    scale: {
+      control: 'select',
+      options: [50, 100, 200, 300, 400, 500, 600, 700, 800, 900],
     },
     // loadingSize: { control: 'text' },
     children: { control: 'text' },
     isLoading: { control: 'select', options: [true, false] },
     disabled: { control: 'select', options: [true, false] },
+    href: { control: 'text' },
   },
 };
 
@@ -52,32 +59,33 @@ Default.args = {
   size: 'medium',
   stretch: false,
   variant: 'contained',
-  color: 'purple',
+  color: 'deepPurple',
+  scale: 500,
   children: 'button',
   isLoading: false,
   disabled: false,
 };
 
-// Loading.args = {
-//   theme: 'purple',
-//   borderRadius: 'small',
-//   size: 'm',
-//   stretch: false,
-//   loadingColor: colors.purple[100],
-//   loadingType: 'spin',
-//   loadingSize: '30px',
-//   children: 'hi',
-//   isLoading: true,
-// };
+Loading.args = {
+  theme: 'light',
+  size: 'medium',
+  stretch: false,
+  variant: 'contained',
+  color: 'deepPurple',
+  scale: 500,
+  children: 'button',
+  isLoading: true,
+  disabled: false,
+};
 
-// Disabled.args = {
-//   theme: 'lightGray',
-//   borderRadius: 'small',
-//   size: 'm',
-//   stretch: false,
-//   loadingColor: colors.purple[100],
-//   loadingType: 'spin',
-//   loadingSize: '30px',
-//   children: '버튼클릭안됨',
-//   disabled: true,
-// };
+Disabled.args = {
+  theme: 'light',
+  size: 'medium',
+  stretch: false,
+  variant: 'contained',
+  color: 'deepPurple',
+  scale: 500,
+  children: 'button',
+  isLoading: false,
+  disabled: true,
+};
--- docs/src/components/Loader.stories.tsx
+++ docs/src/components/Loader.stories.tsx
@@ -6,19 +6,40 @@ export default {
   title: 'Core/Loader',
   component: Loader,
   argTypes: {
+    theme: {
+      control: 'select',
+      options: ['light', 'dark'],
+    },
     type: {
       control: 'select',
       options: ['blank', 'bubbles', 'cylon', 'spin', 'spinningBubbles', 'spokes'],
     },
     color: {
-      control: 'color',
-      //   presetsColors: [theme.color.green],
+      control: 'select',
+      options: [
+        'red',
+        'pink',
+        'purple',
+        'deepPurple',
+        'indigo',
+        'blue',
+        'lightBlue',
+        'green',
+        'lightGreen',
+        'lime',
+        'yellow',
+        'amber',
+        'orange',
+        'deepOrange',
+        'brown',
+        'gray',
+        '#FFB3B3',
+        'rgb(63, 167, 150)',
+      ],
     },
-    margin: {
-      control: 'text',
-    },
-    padding: {
-      control: 'text',
+    scale: {
+      control: 'select',
+      options: [50, 100, 200, 300, 400, 500, 600, 700, 800, 900],
     },
     size: {
       control: 'text',
@@ -31,9 +52,9 @@ const Template = (args: LoadingProps) => <Loader {...args} />;
 export const Default = Template.bind({});
 
 Default.args = {
+  theme: 'light',
   type: 'spin',
-  // color: '#00CE7C',
-  // margin: '0 auto',
-  // padding: '1rem',
-  // size: '30px',
+  color: 'deepPurple',
+  scale: 500,
+  size: '30px',
 };
